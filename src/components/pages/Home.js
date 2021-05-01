80e4d1b7b03bc1833ee6a9be71a51c5e66167b41
Eldhose-Peter <eldhosepeter2000@gmail.com>
2021-05-01 19:13:38 +0530

    Added Footer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,12 +3,14 @@ import HeroSection from '../HeroSection'
 
 import React from 'react'
 import Cards from '../Cards'
+import Footer from '../Footer'
 
 function Home() {
     return (
         <>
           <HeroSection/>  
           <Cards/>
+          <Footer/>
         </>
     )
 }
